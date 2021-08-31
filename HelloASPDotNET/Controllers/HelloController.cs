1bb7f47c8a9cf7aae5b63b59069ba56981e59543
Donna Seidel <dseidel01@gmail.com>
2021-08-31 17:01:08 -0500

    add new controller add  action method using conventional and attribute routing

---
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloASPDotNET.Controllers
+{
+    public class HelloController : Controller
+    {
+        //GET: /<controller>/
+        [HttpGet]
+        [Route("/helloworld/")]
+        public IActionResult Index()
+        {
+            string html = "<h1>Hello World!</h1>";
+            return Content(html, "text/html");
+        }
+    }
+}
