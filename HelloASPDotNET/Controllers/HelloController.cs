e8b6bf03ea680f47dc0c345d51cb9622d5224689
Donna Seidel <dseidel01@gmail.com>
2021-08-31 17:59:31 -0500

    send form data

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -13,13 +13,18 @@ namespace HelloASPDotNET.Controllers
         [Route("/helloworld/")]
         public IActionResult Index()
         {
-            string html = "<h1>Hello World!</h1>";
+            string html = "<form method='post' action='/helloworld/'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!' />" +
+                "</form>";
             return Content(html, "text/html");
         }
 
         //GET: /hellow/welcome
-        [HttpGet]
-        [Route("/helloworld/welcome/{name?}")] //curly braces designates the VALUE of the variable, ? designates the value is optional
+        //[HttpGet]
+        //[Route("/helloworld/welcome/{name?}")] //curly braces designates the VALUE of the variable, ? designates the value is optional
+        [HttpPost]
+        [Route("/helloworld")]
         public IActionResult Welcome(string name = "World") //default parameter value
         {
             return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
