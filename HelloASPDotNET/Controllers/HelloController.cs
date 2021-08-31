3206d6c55021c86254497a1299d487bef580f7de
Donna Seidel <dseidel01@gmail.com>
2021-08-31 17:47:35 -0500

    dev

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -20,7 +20,7 @@ namespace HelloASPDotNET.Controllers
         //GET: /hellow/welcome
         [HttpGet]
         [Route("/helloworld/welcome/{name?}")] //curly braces designates the VALUE of the variable, ? designates the value is optional
-        public IActionResult Welcome(string name = "World")
+        public IActionResult Welcome(string name = "World") //default parameter value
         {
             return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
         }
